0d7a9f71d223090e01dbb85589045c607100f52b
andreasv99 <44083123+andreasv99@users.noreply.github.com>
2019-12-31 14:41:13 +0200

    Add initial code

---
+++ vrp/Node.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vrp;
+
+/**
+ *
+ * @author mzaxa
+ */
+public class Node {
+    
+        int x;
+        int y;
+        int ID;
+        boolean isRouted;
+        double demand;
+        double serviceTime;
+        
+        public Node() 
+        {
+        }
+}
